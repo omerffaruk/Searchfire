d74b5aec124298848c61282bb434824ce869ff02
omer <ffaruk.yamak@gmail.com>
2022-05-10 15:42:51 +0100

    restructure html, style

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
-  <router-view />
+  <header></header>
+  <main class="main">
+    <router-view />
+  </main>
+  <footer></footer>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: futura-pt, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin: 0;
+  padding: 0;
+  background-color: #efefef4b;
+}
+
+.main {
+  width: 90%;
+  max-width: 1440px;
+  margin: 0 auto;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="home">
-    <SearchBar />
-    <ResultsSet />
-  </div>
+  <SearchBar />
+  <ResultsSet />
 </template>
 
 <script>

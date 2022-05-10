a0d8891c3558eae12ead1d44f51bc0d6ca4aa0c1
omer <ffaruk.yamak@gmail.com>
2022-05-10 10:09:12 +0100

    create state, actions, mutations and getters

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,24 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+
+const clientId = process.env.VUE_APP_CLIENT_ID;
+const apiUrl = process.env.VUE_APP_API_URL;
+const fetchUrl = `${apiUrl}/searcha?client_id=${clientId}/`;
 
 export default createStore({
   state: {
+    products: [],
   },
   getters: {
-  },
-  mutations: {
+    allProducts: (state) => state.products,
   },
   actions: {
+    async fetchProducts({ commit }, query = "", limit = 12, page = 1) {
+      fetch(`${fetchUrl}&query=${query}&limit=${limit}&page=${page}`)
+        .then((res) => res.json())
+        .then((data) => commit("setProducts", data.products));
+    },
   },
-  modules: {
-  }
-})
+  mutations: {
+    setProducts: (state, products) => (state.products = products),
+  },
+});

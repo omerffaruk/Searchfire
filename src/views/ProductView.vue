3c206b1188a7977a09aeee9ca43a7a2975eeeaf5
omer <ffaruk.yamak@gmail.com>
2022-05-13 17:04:43 +0100

    improve button styling, closes #15

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -10,9 +10,9 @@
       <p>{{ product.in_stock ? "Available" : "Currently not in stock" }}</p>
       <div class="button-container">
         <div class="plus-minus-amount">
-          <button @click="increaseQuantity">+</button>
-          <p>{{ quantity }}</p>
-          <button @click="decreaseQuantity">-</button>
+          <button class="btn" @click="increaseQuantity">+</button>
+          <p class="quantity">{{ quantity }}</p>
+          <button class="btn" @click="decreaseQuantity">-</button>
         </div>
         <div class="add-cart-button">
           <button>Add to cart</button>
@@ -99,17 +99,13 @@ export default {
 
     .plus-minus-amount {
       display: flex;
-      justify-content: space-between;
+      justify-content: space-evenly;
       align-items: center;
       width: 100%;
 
       & > * {
         padding: 10px 30px;
         font-size: 1rem;
-        border: 1px solid black;
-      }
-      button:hover {
-        cursor: pointer;
       }
     }
     .add-cart-button > button {
@@ -131,6 +127,39 @@ export default {
       }
     }
   }
+}
+
+.btn,
+.quantity {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  border-radius: 24px;
+  border: none;
+  box-shadow: rgba(0, 0, 0, 0.2) 0 3px 5px -1px,
+    rgba(0, 0, 0, 0.14) 0 6px 10px 0, rgba(0, 0, 0, 0.12) 0 1px 18px 0;
+  color: #3c4043;
+  cursor: pointer;
+  font-family: "Google Sans", Roboto, Arial, sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  height: 48px;
+  max-width: 100%;
+  padding: 2px 24px;
+  margin-right: 10px;
+}
+
+.btn:hover {
+  background: #f6f9fe;
+  color: #174ea6;
+  cursor: pointer;
+}
+
+.btn:active {
+  box-shadow: 0 4px 4px 0 rgb(60 64 67 / 30%),
+    0 8px 12px 6px rgb(60 64 67 / 15%);
+  outline: none;
 }
 @media screen and (min-width: 992px) {
   .product-page-grid {
